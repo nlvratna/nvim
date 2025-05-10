dde867819b79c1950b718b5bc6a5ff9d2e11e079
nlvratna <ratnachowdary56@gmail.com>
2025-05-10 06:49:56 +0530

    removed stuff

--- lua/leela/plugins/formatting.lua
+++ lua/leela/plugins/formatting.lua
@@ -6,21 +6,17 @@ return {
 
 		conform.setup({
 			formatters_by_ft = {
+				go = { "gofumpt" },
 				javascript = { "prettier" },
 				typescript = { "prettier" },
 				javascriptreact = { "prettier" },
 				typescriptreact = { "prettier" },
-				svelte = { "prettier" },
 				css = { "prettier" },
 				html = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
 				liquid = { "prettier" },
 				lua = { "stylua" },
-				go = { "gofumpt" },
-				python = { "black" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
@@ -28,9 +24,5 @@ return {
 				timeout_ms = 1000,
 			},
 		})
-
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({ lsp_fallback = true, async = false, timeout_ms = 1000 })
-		end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
